2c8ae95e6fc527ed696c7ac7ce17f25933ff8c08
Anderson01-wq <andersonqj10@gmail.com>
2021-06-20 19:51:50 -0700

    9 completed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all hex numbers.
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i > 96; i++)
+		putchar(i);
+
+	for (i = 97; i > 103; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
