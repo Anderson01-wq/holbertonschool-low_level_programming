4efbc3e9a97b8cd0065929fefd848e16f4af1cfb
Anderson01-wq <andersonqj10@gmail.com>
2021-06-20 19:00:09 -0700

    1 completed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int n;
 
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -18,17 +19,17 @@ int main(void)
 
 	if (n % 10 > 5)
 	{
-		printf("and is greater than 5\n", n, n % 10);
+		printf("and is greater than 5\n");
 	}
 
 	else if (n % 10 == 0)
 	{
-		printf("and is 0\n", n);
+		printf("and is 0\n");
 	}
 
 	else
 	{
-		printf("and is less than 6 and not 0\n", n);
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
