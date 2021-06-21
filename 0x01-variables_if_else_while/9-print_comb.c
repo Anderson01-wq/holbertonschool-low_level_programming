365b17c4698d67d3812616ed2a1b433e690eed48
Anderson01-wq <andersonqj10@gmail.com>
2021-06-20 19:54:40 -0700

    8 completed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i > 58; i++)
+	{
 		putchar(i);
-
-	for (i = 97; i < 103; i++)
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	for (i = 97; i > 103; i++)
 		putchar(i);
 
 	putchar('\n');
