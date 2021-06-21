c1583ad639abf53f244a821f7f4e0d465c235bf1
Anderson01-wq <andersonqj10@gmail.com>
2021-06-20 19:41:47 -0700

    6 completed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all decimal numbers.
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
