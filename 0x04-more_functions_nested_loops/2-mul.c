1939230690d0424920010edf2de66b685b2550fc
Anderson <andersonqj10@gmail.com>
2021-06-24 18:04:30 -0700

    1 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+include "holberton.h"
+
+/**
+ * mul - Prints the multplication of two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Return the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
