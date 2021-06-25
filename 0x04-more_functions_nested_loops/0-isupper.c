c89676c68a4b3778cc57d09fe4c30d859002ff49
Anderson <andersonqj10@gmail.com>
2021-06-24 17:58:38 -0700

    0 done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * _isdigit - Check if value is digit
- * @c Value of the char
- * Return: 1 if digit, otherwise 0 if not.
+ * _isupper - Check if value is uuppercase
+ * @c: Value of the char
+ * Return: 1 if upper, otherwise 0 if not.
  */
 
 int _isupper(int c)
